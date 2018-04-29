7c3c507a9b4ca0055f758877e09e56dd42789498
Christo Fogelberg <cfogelberg@gmail.com>
2018-04-29 18:11:16 +0100

    Update scripts to include wlan1 (Speedcheck notebook not yet including)

--- copy-from-pi.sh
+++ copy-from-pi.sh
@@ -2,5 +2,8 @@
 
 cd "$(dirname "$0")"
 
-rsync pi@192.168.1.14:/home/pi/speedcheck/transfer-results.csv ./
-rsync pi@192.168.1.14:/home/pi/speedcheck/lan-results.csv ./
+REMOTE_IP=rpi_3e5ccf
+
+rsync pi@$REMOTE_IP:/home/pi/speedcheck/transfer-results.csv ./
+rsync pi@$REMOTE_IP:/home/pi/speedcheck/eth0-results.csv ./
+rsync pi@$REMOTE_IP:/home/pi/speedcheck/wlan1-results.csv ./
---
+++ rpi-scripts/check-wlan1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+INTERFACE=wlan1
+
+DURATION=60
+START=$(date '+%Y-%m-%dT%H:%M:%S.%6N %Z')
+NUM_PACKETS=$(/usr/bin/tshark -i $INTERFACE -a duration:$DURATION | wc -l)
+
+echo $START,$NUM_PACKETS,$DURATION,$INTERFACE >> $INTERFACE-results.csv
